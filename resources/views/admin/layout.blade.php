f933b3099bf098fd8eadbb1016d436477d54feff
Amit Singh <abcdamit44@gmail.com>
2021-08-14 17:47:53 +0530

    Content-Security-Policy meta tag added

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <!-- Title Page-->
     <title>Login</title>
