fc3fcb97ee2b3ae1381f9f79a66a43f988837846
Amit Singh <abcdamit44@gmail.com>
2021-08-14 17:50:56 +0530

    requested an insecure stylesheet error

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -11,14 +11,14 @@
     <title>Login</title>
 
     <!-- Fontfaces CSS-->
-    <link href="{{ asset('admin_assets/css/font-face.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('admin_assets/vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('admin_assets/vendor/font-awesome-5/css/fontawesome-all.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('admin_assets/vendor/mdi-font/css/material-design-iconic-font.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('//admin_assets/css/font-face.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('//admin_assets/vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('//admin_assets/vendor/font-awesome-5/css/fontawesome-all.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('//admin_assets/vendor/mdi-font/css/material-design-iconic-font.min.css') }}" rel="stylesheet" media="all">
 
     <!-- Bootstrap CSS-->
-    <link href="{{ asset('admin_assets/vendor/bootstrap-4.1/bootstrap.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('admin_assets/css/theme.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('//admin_assets/vendor/bootstrap-4.1/bootstrap.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('//admin_assets/css/theme.css') }}" rel="stylesheet" media="all">
 
 
 </head>
